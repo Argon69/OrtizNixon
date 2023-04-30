ad7810fb487b044d3dd9f995846921071cb4a7a3
Argon <nixonelpoeta13@gmail.com>
2023-04-30 18:56:15 -0500

    cambios en footer

--- foot.php
+++ foot.php
@@ -1,23 +1,23 @@
 <div id="eys-footer">
-  <div class="footer-columns">
-    <div class="footer-column">
-      <ul class="icons">
+  <div style="display: flex; flex-wrap: wrap; align-items: center; justify-content: space-between; padding: 1rem;">
+    <div style="flex: 1;">
+      <ul style="list-style: none; margin: 0; padding: 0;">
         <li><a href="mailto:educaysoft%40gmail.com" class="icon fa-envelope" title="Email"><span class="label">Capacitación</span></a></li>
         <li><a href="https://twitter.com/home?lang=es" class="icon fa-twitter" target="_blank" title="Twitter"><span class="label">Asesoría técnica</span></a></li>
         <li><a href="https://www.facebook.com/stalin.francis.7/" class="icon fa-facebook" target="_blank" title="Facebook"><span class="label">Investigación</span></a></li>
-        <li><a href="" class="icon fa-linkedin" target="_blank" title="LinkedIn"><span class="label">Desarrollo Web</span></a></li>
+        <li><a href="" class="icon fa-linkedin" target="_blank" title="LinkedIn"><span class="label">Desarrollo web</span></a></li>
       </ul>
     </div>
-    <div class="footer-column">
-      <ul class="icons">
+    <div style="flex: 1;">
+      <ul style="list-style: none; margin: 0; padding: 0;">
         <li><a href="mailto:educaysoft%40gmail.com" class="icon fa-envelope" title="Email"><span class="label">Email</span></a></li>
         <li><a href="https://twitter.com/home?lang=es" class="icon fa-twitter" target="_blank" title="Twitter"><span class="label">Twitter</span></a></li>
         <li><a href="https://www.facebook.com/stalin.francis.7/" class="icon fa-facebook" target="_blank" title="Facebook"><span class="label">Facebook</span></a></li>
         <li><a href="" class="icon fa-linkedin" target="_blank" title="LinkedIn"><span class="label">LinkedIn</span></a></li>
       </ul>
     </div>
-    <div class="footer-column">
-      <ul class="copyright">
+    <div style="flex: 1;">
+      <ul style="list-style: none; margin: 0; padding: 0; text-align: right;">
         <li><a href="http://educaysoft.org/educaysoft/index.php">EDUCAYSOFT</a></li>
         <li>&copy; Francis Quinde Stalin Adalberto</li>
         <li>Design: <a href="//html5up.net">HTML5 UP</a></li>
@@ -25,6 +25,7 @@
     </div>
   </div>
 </div>
+
 
 </div>
         <!-- Scripts -->
@@ -41,51 +42,5 @@
 </html>
 
 <style>
-        #eys-footer {
-                background-color: #f8f8f8;
-                color: #666;
-                padding: 20px 0;
-        }
-        
-        .footer-columns {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-between;
-                max-width: 1200px;
-                margin: 0 auto;
-        }
-        
-        .footer-column {
-                flex-basis: calc(33.33% - 20px);
-                margin-bottom: 20px;
-                text-align: left;
-        }
-        
-        .icons {
-                list-style: none;
-                margin: 0;
-                padding: 0;
-        }
-        
-        .icons li {
-                display: inline-block;
-                margin-right: 10px;
-        }
-        .icons li:last-child {
-                margin-right: 0;
-        }
-        .icon {
-                color: #666;
-                display: inline-block;
-                font-size: 1.5em;
-                line-height: 1.5em;
-                text-align: center;
-                text-decoration: none;
-                width: 2.5em;
-        }
-
-        .fa-envelope:before {
-                content: "\f0e0";
-        }
-
+   
 </style>
